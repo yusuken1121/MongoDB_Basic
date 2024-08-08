dafc973179d77dd28171dee213957f9b0fbc64bd
yusuken1121 <yusuken1121@gmail.com>
2024-08-08 11:01:06 -0700

    Added a middleware

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,14 @@
 import express, { Request, Response } from "express";
+import cors from "cors";
+import { router as userRouter } from "./routes/users.router";
 export const app = express();
 
 // middleware
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+app.use("/users", userRouter);
 
 app.all("*", (req: Request, res: Response) => {
   res.status(404).json({ error: "Invalid endpoint" });
