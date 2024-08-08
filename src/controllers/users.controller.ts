73c282544d6e21918792cee777f40d2f386a91c2
yusuken1121 <yusuken1121@gmail.com>
2024-08-08 11:14:00 -0700

    ✨ Added Read method in users.controller

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import { User } from "../models/users.model";
 
+//create
 export const createUser = async (req: Request, res: Response) => {
   const { email, password } = req.body;
   const user = new User({
@@ -15,3 +16,16 @@ export const createUser = async (req: Request, res: Response) => {
     res.status(501).json("Cannot create a new user");
   }
 };
+
+//Read
+export const getUsers = async (req: Request, res: Response) => {
+  try {
+    const users = await User.find({});
+    res.status(200).json(users);
+  } catch (error) {
+    console.error("Internal Server Error", error);
+    res.status(501).json("Cannot create a new user");
+  }
+};
+
+//
--- src/routes/users.router.ts
+++ src/routes/users.router.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response, Router } from "express";
-import { createUser } from "../controllers/users.controller";
+import { createUser, getUsers } from "../controllers/users.controller";
 
 export const router = Router();
 
 router.post("/", createUser);
+router.get("/", getUsers);
